726a210d7fb046985efede766e06ecb1af3d5350
Iroegbu David <iroegbu.dgit@gmail.com>
2023-11-28 07:34:21 +0100

    fuck you

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name: { type: String, trim: true, required: true },
     email: { type: String, trim: true, required: true, unique: true },
     phone: { type: Number, trim: true },
-    pic: { type: String, trim: true, default: 'http://here' },
+    pic: { type: String, trim: true, default: 'http://here.' },
     role: { type: String, enum: ["CUSTOMER", "STORE MANAGER", "SALES PERSON", "BRANCH MANAGER", "ADMIN"] },
     branch: { type: mongoose.Types.ObjectId, ref: "Branch" }
 
